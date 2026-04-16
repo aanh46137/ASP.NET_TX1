b06ceb59b69b83601fb5c2ae044050d208f6e233
aanh46137 <aanh46137@gmail.com>
2026-04-16 23:53:13 +0700

    Add files via upload

---
+++ LuyenTapTX1_1/Sanpham.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Web;
+
+namespace LuyenTapTX1_p1.Models
+{
+    public class Sanpham
+    {
+        public Sanpham() { }
+        public Sanpham(string masp, string hangsx, string tensp, int soluong, double giatien)
+        {
+            this.masp = masp;
+            this.hangsx = hangsx;
+            this.tensp = tensp;
+            this.soluong = soluong;
+            this.giatien = giatien;
+        }
+
+        public string masp {  get; set; }
+        public string hangsx { get; set; }
+        public string tensp { get; set; }
+        public int soluong { get; set; }
+        public double giatien {  get; set; }
+        public double tongtien
+        {
+            get => soluong * giatien;
+        }
+    }
+}
---
+++ LuyenTapTX1_1/SanphamsController.cs
@@ -0,0 +1,46 @@
+﻿using LuyenTapTX1_p1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LuyenTapTX1_p1.Controllers
+{
+    public class SanphamsController : Controller
+    {
+        // GET: Sanphams
+        List<Sanpham> danhsach;
+
+        public SanphamsController()
+        {
+            danhsach = new List<Sanpham>
+            {
+                new Sanpham("sp1", "Apple", "Iphone X", 20, 200),
+                new Sanpham("sp2", "Xiaomi", "Xiaomi Ultra 15", 10, 500),
+                new Sanpham("sp3", "Apple", "Apple", 30, 320),
+                new Sanpham("sp4", "Apple", "Iphone 15", 40, 300),
+                new Sanpham("sp5", "Xiaomi", "Xiaomi 14 pro", 10, 200),
+                new Sanpham("sp6", "Xiaomi", "Xiaomi 15 pro", 15, 250),
+            };
+        }
+        public ActionResult Index()
+        {
+            ViewBag.ds1 = danhsach.Where(sp => sp.giatien >= 250).ToList();
+            ViewBag.ds2 = danhsach.Where(sp => sp.hangsx == "Xiaomi").ToList();
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Input()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Output(Sanpham sanpham)
+        {
+            return View(sanpham);
+        }
+    }
+}
