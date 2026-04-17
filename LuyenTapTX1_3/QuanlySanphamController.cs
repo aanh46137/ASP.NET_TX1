59b0306a8a09ebb668abe6587664ac8fe3a75cf9
aanh46137 <aanh46137@gmail.com>
2026-04-18 00:22:06 +0700

    Add files via upload

---
+++ LuyenTapTX1_3/QuanlySanphamController.cs
@@ -0,0 +1,43 @@
+﻿using LuyenTapTX1_p4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LuyenTapTX1_p4.Controllers
+{
+    public class QuanlySanphamController : Controller
+    {
+        // GET: QuanlySanpham
+        List<Sanpham> danhsach;
+        public QuanlySanphamController()
+        {
+            danhsach = new List<Sanpham>
+            {
+                new Sanpham("S01", "Sản phẩm 1", 10, 100, 0),
+                new Sanpham("S02", "Sản phẩm 2", 20, 120, 1),
+                new Sanpham("S03", "Sản phẩm 3", 15, 200, 1),
+                new Sanpham("S04", "Sản phẩm 4", 30, 150, 0),
+                new Sanpham("S05", "Sản phẩm 5", 20, 50, 1),
+            };
+        }
+        public ActionResult Index()
+        {
+            ViewBag.ds1 = danhsach.Where( sp => sp.giatien > 100).ToList();
+            ViewBag.ds2 = danhsach.Where( sp => sp.giamgia == 1).ToList();
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Input()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Output(Sanpham sanpham)
+        {
+            return View(sanpham);
+        }
+    }
+}
---
+++ LuyenTapTX1_3/Sanpham.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+using System.Web.DynamicData;
+
+namespace LuyenTapTX1_p4.Models
+{
+    public class Sanpham
+    {
+        public Sanpham() { }
+        public Sanpham(string masp, string tensanpham, int soluong, double giatien, int giamgia)
+        {
+            this.masp = masp;
+            this.tensanpham = tensanpham;
+            this.soluong = soluong;
+            this.giatien = giatien;
+            this.giamgia = giamgia;
+        }
+
+        public string masp {  get; set; }
+        public string tensanpham { get; set; }
+        public int soluong {  get; set; }
+        public double giatien { get; set; }
+        public int giamgia { get; set; }
+        public double thanhtien
+        {
+            get
+            {
+                if ( giamgia == 1)
+                {
+                    return soluong * giatien * 0.9;
+                } else {
+                    return soluong * giatien;
+            }
+        }
+}
+    }
+}
