f818cd9cb0ef83ec7e428e10d06667af619001b0
aanh46137 <aanh46137@gmail.com>
2026-04-17 23:24:08 +0700

    Add files via upload

---
+++ LuyenTapTX1_p2/Nhanvien.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Web;
+
+namespace LuyenTapTX1_p3.Models
+{
+    public class Nhanvien
+    {
+        public Nhanvien() { }
+        public Nhanvien(string manv, string hoten, string diachi, int songaylam, double luongngay)
+        {
+            this.manv = manv;
+            this.hoten = hoten;
+            this.diachi = diachi;
+            this.songaylam = songaylam;
+            this.luongngay = luongngay;
+        }
+
+        public string manv { get; set; }
+        public string hoten { get; set; }
+        public string diachi { get; set; }
+        public int songaylam { get; set; }
+        public double luongngay { get; set; }
+        public double tienluong
+        {
+            get
+            {
+                return songaylam * luongngay;
+            }
+        }
+    }
+}
---
+++ LuyenTapTX1_p2/QunalyNhanvienController.cs
@@ -0,0 +1,44 @@
+﻿using LuyenTapTX1_p3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LuyenTapTX1_p3.Controllers
+{
+    public class QunalyNhanvienController : Controller
+    {
+        // GET: QunalyNhanvien
+        List<Nhanvien> danhsach;
+        public QunalyNhanvienController()
+        {
+            danhsach = new List<Nhanvien>
+            {
+                new Nhanvien("Nv01", "Nguyễn Vân Anh", "Hà Nội", 15, 200000),
+                new Nhanvien("Nv02", "Lê Thu Hà", "Hải Phòng", 27, 250000),
+                new Nhanvien("Nv03", "Nguyễn Văn Hoàng", "Hà Nội", 18, 250000),
+                new Nhanvien("Nv04", "Trần Thu Hương", "Hải Phòng", 25, 190000),
+                new Nhanvien("Nv05", "Ngô Phương Thảo", "Quảng Ninh", 20, 180000)
+            };
+        }
+        public ActionResult Index()
+        {
+            ViewBag.ds1 = danhsach.Where( nv => nv.songaylam < 20).ToList();
+            ViewBag.ds2 = danhsach.Where(nv => nv.luongngay > 190000).ToList();
+            ViewBag.ds3 = danhsach.Where(nv => nv.diachi.Equals("Hà Nội", StringComparison.OrdinalIgnoreCase));
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Input()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Output(Nhanvien nhanvien)
+        {
+            return View(nhanvien);
+        }
+    }
+}
